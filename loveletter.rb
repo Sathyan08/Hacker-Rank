d772905573db527841d70b0e4c6fcf4419f0dc1a
Sathyan Mathai <Sathyan.Mathai@gmail.com>
2015-02-10 15:36:40 -0500

    added solution for the loveletter.rbchallenge

--- loveletter.rb
+++ loveletter.rb
@@ -1,18 +1,5 @@
-# # this code solves the challenge at https://www.hackerrank.com/challenges/the-love-letter-mystery
+# this code solves the challenge at https://www.hackerrank.com/challenges/the-love-letter-mystery
 
-# #1. find the midpoint
-# mid = s.length / 2
-
-# #2. get the first half and the second half.  Flip the second half
-
-# first = s.chars[0..mid]
-# second = s.chars[mid..-1].reverse
-
-# #3. Zip the first half and second half.  Determine which char is higher
-
-# first.zip(second)
-
-#4. Call .next on the lower value until it == the higher value.  Increase counter by one.
 def distance(a,b)
   a > b ? (higher, lower = a, b) : (higher, lower = b, a)
   counter = 0
@@ -24,3 +11,18 @@ def distance(a,b)
 
   counter
 end
+
+def min_palindrome(s)
+  mid = s.length / 2 - 1
+  if s.length.even?
+    first, second = s.chars[0..mid], s.chars[(mid + 1)..-1].reverse
+  else
+    first, second = s.chars[0..(mid)], s.chars[(mid + 2)..-1].reverse
+  end
+
+  matches = first.zip(second)
+  matches.inject(0) { |sum, match| sum + distance(match[0], match[1]) }
+end
+
+inputs = gets.strip.to_i
+inputs.times { puts min_palindrome(gets.strip) }
