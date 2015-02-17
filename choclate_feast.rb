be32838f40cfa500e34486ba797a846d10e54339
Sathyan Mathai <Sathyan.Mathai@gmail.com>
2015-02-16 23:51:20 -0500

    added first version of solution to the chocolates problem

---
+++ choclate_feast.rb
@@ -0,0 +1,29 @@
+def total_chocolates(money, cost, discount)
+  chocolate = 0
+  wrappers = 0
+
+  until money == 0
+    money -= cost
+    chocolate, wrappers = unwrap_choclate(chocolate, wrappers)
+    chocolate, wrappers = check_wrappers(chocolate, wrappers, discount)
+  end
+
+  chocolate, wrappers = check_wrappers(chocolate, wrappers, discount)
+
+  chocolate
+end
+
+def unwrap_choclate(chocolate, wrappers)
+  chocolate += 1
+  wrappers += 1
+  return chocolate, wrappers
+end
+
+def check_wrappers(chocolate, wrappers, discount)
+  if wrappers == discount
+    chocolate += 1
+    wrappers = 1
+  end
+
+  return chocolate, wrappers
+end
