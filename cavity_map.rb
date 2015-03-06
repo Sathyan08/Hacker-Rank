f9097fb7e0c8c83fa875793dc7b3dc29bd62ff5c
Sathyan Mathai <Sathyan.Mathai@gmail.com>
2015-03-06 14:39:18 -0500

    wrote partial solution to cavity_map challenge

---
+++ cavity_map.rb
@@ -0,0 +1,43 @@
+class Map
+  attr_reader :spaces
+
+  def initialize(n)
+    @spaces = []
+    @dimensions = n
+  end
+
+  def add_spaces(nums_string)
+    nums_string.each_char { |num| @spaces << num.to_i  }
+  end
+
+  def space(row, col)
+    spaces[@dimensions * row + col]
+  end
+
+  def edge?(row, col)
+    (row == 0 || row == @dimensions) || (col == 0 || col == @dimensions)
+  end
+
+  def adjacents(row, col)
+    [space(row - 1, col), space(row + 1, col), space(row, col - 1), space(row, col + 1)]
+  end
+
+  def cavity?(row, col)
+    cav = space(row, col)
+    adjacents(row, col).all? { |depth| depth < cav }
+  end
+
+  def to_row_and_column(index)
+    col = index % @dimensions
+    row = index - col
+    row, col
+  end
+
+  def find_cavities
+    @spaces.each_with_index do |space, index|
+      row, col = to_row_and_column(index)
+      space
+    end
+  end
+
+end
